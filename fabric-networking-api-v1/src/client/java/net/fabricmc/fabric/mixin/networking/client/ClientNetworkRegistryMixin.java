e76757cbcd1273a24f182bd2dc02eeab98daaae5
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-07-11 19:52:37 +0800

    clean

--- fabric-networking-api-v1/src/client/java/net/fabricmc/fabric/mixin/networking/client/ClientNetworkRegistryMixin.java
+++ fabric-networking-api-v1/src/client/java/net/fabricmc/fabric/mixin/networking/client/ClientNetworkRegistryMixin.java
@@ -1,8 +1,13 @@
 package net.fabricmc.fabric.mixin.networking.client;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationConnectionEvents;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
 import net.minecraft.network.protocol.common.ClientCommonPacketListener;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.configuration.ClientConfigurationPacketListener;
 import net.neoforged.neoforge.client.network.registration.ClientNetworkRegistry;
+import net.neoforged.neoforge.network.registration.NetworkPayloadSetup;
 import org.sinytra.fabric.networking_api.NeoNetworkRegistrar;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,5 +21,10 @@ public class ClientNetworkRegistryMixin {
         if (NeoNetworkRegistrar.hasCodecFor(listener.protocol(), packet.type().flow(), packet.payload().type().id())) {
             info.cancel();
         }
+    }
+
+    @Inject(method = "initializeNeoForgeConnection(Lnet/minecraft/network/protocol/configuration/ClientConfigurationPacketListener;Lnet/neoforged/neoforge/network/registration/NetworkPayloadSetup;)V", at = @At("TAIL"))
+    private static void startConfiguration(ClientConfigurationPacketListener listener, NetworkPayloadSetup setup, CallbackInfo ci) {
+        ClientConfigurationConnectionEvents.START.invoker().onConfigurationStart((ClientConfigurationPacketListenerImpl) listener, Minecraft.getInstance());
     }
 }
